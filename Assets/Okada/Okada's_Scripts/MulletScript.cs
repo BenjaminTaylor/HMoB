ade149442e4abefc9695e45b3045cfb658a6d9c2
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2023-02-05 22:12:28 +0900

    Added MulletScript.cs .

---
+++ Assets/Okada/Okada's_Scripts/MulletScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulletScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody _ballRb;
+
+    [SerializeField] float _force;
+
+    Object_Type _objectType;
+
+    
+    void OnCollisionEnter(Collision enterCol)
+    {
+        
+        _objectType = enterCol.gameObject.GetComponent<Object_Type>();
+
+        if (_objectType.Type == ObjectType.Ball)
+        {
+           _ballRb = enterCol.gameObject.GetComponent<Rigidbody>();
+            Vector3 direction = enterCol.transform.position - transform.position;
+            _ballRb.AddForce(direction* _force, ForceMode.Impulse); 
+           
+        }
+    }
+
+}
